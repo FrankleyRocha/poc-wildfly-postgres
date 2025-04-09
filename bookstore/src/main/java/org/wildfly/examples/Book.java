f843887c5b90e55c1356fa0acd15ff7285ef809c
Frankley Francalino da Rocha <frankley.rocha@tse.jus.br>
2025-04-09 15:04:33 -0300

    Book Entity

--- bookstore/src/main/java/org/wildfly/examples/Book.java
+++ bookstore/src/main/java/org/wildfly/examples/Book.java
@@ -16,7 +16,7 @@ import lombok.Data;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
